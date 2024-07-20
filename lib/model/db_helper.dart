84d1b167f0bafc58d590cf9df7ab7dca645d1f0a
José Gabriel <112900256+Olotum@users.noreply.github.com>
2024-07-19 22:16:11 -0300

    Update db_helper.dart

--- lib/model/db_helper.dart
+++ lib/model/db_helper.dart
@@ -1,17 +1,24 @@
-import 'package:sqflite/sqflite.dart';
+import 'package:flutter/material.dart';
+import 'package:it_support/model/user.dart';
 
-class DbHelper {
-  static Future<Database> openConnection() async {
-    var path = await getDatabasesPath();
-    var dbName = 'parking_database.db';
-    var dbPath = '$path$dbName';
-    return await openDatabase(
-      dbPath,
-      version: 1,
-      onCreate: (db, version) async {
-        await db.execute(
-            'CREATE TABLE tickets (number INTEGER PRIMARY KEY AUTOINCREMENT,vacancyNumber CHAR(4) NOT NULL,vehicleLicenseNumber CHAR(7) NOT NULL,vehicleType VARCHAR(10),entryTime CHAR(5) NOT NULL, departureTime CHAR(5), amount REAL);');
-      },
-    );
-  }
+class Ticket {
+  int? id;
+  final String issueDescription;
+  final User user;
+  String? requestTime;
+
+  Ticket({
+    this.id,
+    required this.issueDescription,
+    required this.user,
+    this.requestTime,
+  });
+
+  // Para salvar o objeto no banco de dados
+  Map<String, Object?> toMap() => {
+    'id': id,
+    'issueDescription': issueDescription,
+    'userName': user.name,
+    'requestTime': requestTime,
+  };
 }
